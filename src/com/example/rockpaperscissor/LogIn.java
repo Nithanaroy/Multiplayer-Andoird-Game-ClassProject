66730b4bdda88e66d7e37c8aba4060e5c175bede
Nitin Pasumarthy <nithanaroy@gmail.com>
2014-11-22 13:33:53 -0700

    Added user choice screen

--- src/com/example/rockpaperscissor/LogIn.java
+++ src/com/example/rockpaperscissor/LogIn.java
@@ -1,8 +1,8 @@
 package com.example.rockpaperscissor;
 
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,81 +12,78 @@ import android.widget.Toast;
 
 public class LogIn extends ActionBarActivity {
 	public String radioText;
-	static String uname,ag,sex;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    
-    public void onRadioClicked (View v) {
-    	boolean checked = ((RadioButton) v).isChecked();
-    	
-    	switch (v.getId()) {
-    	case R.id.RB_male:
-    		if (checked) {
-    			radioText = "Male";
-    			break;
-    		}
-    	case R.id.RB_female:
-    		if (checked) {
-    			radioText = "Female";
-    			break;
-    		}	
-    	}
-    }
-    
-    public void startGamePlay (View v) {
-    	try
-    	{
-	    	DBhandler dbh = new DBhandler(this, null, null, 1);
-	    	EditText name = (EditText)findViewById(R.id.name_Box_S1);
-	    	EditText age = (EditText)findViewById(R.id.age_Box_S1);
-	    	
-	    	uname=name.getText().toString();
-	    	ag=age.getText().toString();
-	    	sex=radioText;
-	    	
-	
-	    	if(uname.equals("") || ag.equals(null) || sex.equals(null))
-	    	{
-	    		Toast t = Toast.makeText(getApplicationContext(), "Please fill all the details", Toast.LENGTH_LONG);
-	    		t.show();
-	    	}
-	    	else
-	    	{
-	    	
-		    	directory d = new directory(uname,ag,sex,0,0);
-		 	   	
-		    	dbh.addUser(d);
-		 	   	name.setText("");
-		    	age.setText("");
-		    	
-		    	System.out.println("Start of 2nd Activity");
-		    	Intent i = new Intent (LogIn.this, PlayerChoice.class);
-		    	startActivity(i);
-	    	}
-    	}catch(Exception e)
-    	{
-    		System.out.print(e);
-    	}
-    	
-    	
-    }
+	static String uname, ag, sex;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void onRadioClicked(View v) {
+		boolean checked = ((RadioButton) v).isChecked();
+
+		switch (v.getId()) {
+		case R.id.RB_male:
+			if (checked) {
+				radioText = "Male";
+				break;
+			}
+		case R.id.RB_female:
+			if (checked) {
+				radioText = "Female";
+				break;
+			}
+		}
+	}
+
+	public void startGamePlay(View v) {
+		try {
+			DBhandler dbh = new DBhandler(this, null, null, 1);
+			EditText name = (EditText) findViewById(R.id.name_Box_S1);
+			EditText age = (EditText) findViewById(R.id.age_Box_S1);
+
+			uname = name.getText().toString();
+			ag = age.getText().toString();
+			sex = "Male";
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+			if (uname.equals("") || ag.equals(null) || sex.equals(null)) {
+				Toast t = Toast.makeText(getApplicationContext(), "Please fill all the details", Toast.LENGTH_LONG);
+				t.show();
+			} else {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+				directory d = new directory(uname, ag, sex, 0, 0);
+
+				dbh.addUser(d);
+				name.setText("");
+				age.setText("");
+
+				System.out.println("Start of 2nd Activity");
+				// Intent i = new Intent (LogIn.this, PlayerChoice.class);
+				Intent i = new Intent(LogIn.this, SingleMultiChoiceActivity.class);
+				startActivity(i);
+			}
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+
+
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
